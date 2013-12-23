85428c069db14e9bd6401f65cccf4aa9d8b601b8
Chris O'Neil <cconeil5@gmail.com>
2013-12-22 21:27:18 -0500

    Initial Commit

---
+++ ActivityEllipses/CHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CHAppDelegate.h
+//  ActivityEllipses
+//
+//  Created by Chris O'Neil on 12/22/13.
+//  Copyright (c) 2013 Chris O'Neil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ActivityEllipses/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ActivityEllipses
+//
+//  Created by Chris O'Neil on 12/22/13.
+//  Copyright (c) 2013 Chris O'Neil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CHAppDelegate class]));
+    }
+}
